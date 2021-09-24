3ac853b9f284bfcf33f5e78f1dd09d7808ed8811
britneyq <55246952+britneyq@users.noreply.github.com>
2021-09-23 21:47:04 -0400

    Update app.py

--- playground/app.py
+++ playground/app.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
  def index():
      return render_template("index.html")
 
 
-@app.route("/welcome/<string:course>")
-def welcome(course):
-   return render_template("welcome.html", course=course)
+@app.route("/welcome/<string:student_name>")
+def welcome(student_name):
+   return render_template("welcome.html", student_name = student_name)
 
-@app.route("/welcome/<string:course>")
-def doiteach(course):
-   doiteach = (course == "CUS1166") or (course == "CUS615")
-   return render_template("layout.html", course=course, doteach=doteach)
 
-@app.route("/welcome/<string:course>")
-def roster(course):
-   roster = [("John", "A"), ("Tom"," B"), ("Mary", "C"), ("Joe", "D")]
-   return render_template("roster.html", course=course, roster=roster)
+@app.route("/welcome/<int:grade_view>")
+def roster(grade_view):
+   roster = [("John", "A"), ("Tom"," B"), ("Mary", "C"), ("Joe", "D"), ("Bob", "F")]
+   return render_template("roster.html", grade_view = grade_view, roster=roster)
 
 if __name__ == '__main__':
 
